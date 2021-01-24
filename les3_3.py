f84cc2d7ac99100e103f4d39282137dcddaa5a34
marina <druzh_marina@mail.ru>
2021-01-24 12:27:19 +0500

    added and updated files

--- les3_3.py
+++ les3_3.py
@@ -3,10 +3,13 @@
     и возвращает сумму наибольших двух аргументов.
 '''
 def my_f(var_1, var_2, var_3):
-    list = [int(var_1), int(var_2), int(var_3)]
-    list.sort()           #Сортировка списка по возрастанию
-    sum = list[1]+list[2]
-    return f"Сумма 2-х наиольших чисел: {sum}"
+    try:
+        list = [var_1, var_2, var_3]
+        list.sort()           #Сортировка списка по возрастанию
+        sum = list[1]+list[2]
+        return f"Сумма 2-х наиольших чисел: {sum}"
+    except TypeError:
+        return "Enter only a numbers!"
 
 
-print(my_f(4, -6, 20))
+print(my_f(-4, 6, -50))
